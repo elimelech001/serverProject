c501f8e8e732c4ecc0cc584ffe8a2011f3ffc5cd
elimelech001 <aaron5yisroel@gmail.com>
2022-11-20 16:20:07 +0200

    server

---
+++ index.js
@@ -0,0 +1,58 @@
+const http = require("http");
+const fs = require("fs");
+const e = require("express");
+
+const sendfiles = (req, response, type) => (err, data) => {
+  response.writeHead(200, { "content-type": type });
+  response.write(data);
+  return response.end();
+};
+
+const server = http.createServer((request, response) => {
+  if (request.url == "/pages") {
+    fs.readFile("./pages.html", sendfiles(request, response, "text/html"));
+  } else if (request.url == "/pages/about") {
+    fs.readFile("./pagesAbout.html", sendfiles(request, response, "text/html"));
+  } else if (request.url == "/pages/sports") {
+    fs.readFile( "./pagesSports.html",sendfiles(request, response, "text/html")
+    );
+  } else if (request.url == "/files") {
+    fs.readFile("./files.html", sendfiles(request, response, "text/html"));
+  } else if (request.url == "/files/people") {
+    fs.readFile(
+      "./filesPeople.txt",
+      { encoding: "utf8" },
+      sendfiles(request, response, "text/txt")
+    );
+  } else if (request.url == "/files/shops") {
+    fs.readFile(
+      "./filesShops.txt",
+      { encoding: "utf8" },
+      sendfiles(request, response, "text/txt")
+    );
+  } else if (request.url.startsWith("/contacs")) {
+    const id = request.url.split("/")[2];
+    if (id) {
+      fs.readFile("./contacs.json", { encoding: "utf-8" }, (err, data) => {
+        const parse = JSON.parse(data);
+        const findUser = parse.find((e) => e.id == id);
+        if (!findUser) {
+          response.statusCode = 404;
+          response.write("no such user");
+          return response.end();
+        }
+        response.writeHead(200, { "content-type": "text/json" });
+        response.write(JSON.stringify(findUser));
+        return response.end();
+      });
+    } else {
+      fs.readFile("./contacs.json", sendfiles(request, response, "text/json"));
+    }
+  } else {
+    response.statusCode = 404;
+    response.write("no such url");
+    return response.end();
+  }
+});
+
+server.listen(5000, () => console.log("listening"));
